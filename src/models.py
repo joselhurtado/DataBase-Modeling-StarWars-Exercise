8a23ea1f9240d156c5a1f2b02cddbb883a14f50e
Jose Hurtado <hello@hurtadojose.com>
2022-07-28 15:22:38 +0000

    Blog Data Model Exercise July 29

--- src/models.py
+++ src/models.py
@@ -8,21 +8,39 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
+class Character(Base):
+    __tablename__ = 'character'
+    character_id = Column(Integer, primary_key=True)
+    characterName = Column(String(150))
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    planet_id = Column(Integer, primary_key=True)
+    planetName = Column(String(150))
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    starship_id = Column(Integer, primary_key=True)
+    planetName = Column(String(150))
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    available_favs = Column(String)
+    userId = Column(Integer, primary_key=True)
+    character_id = Column(Integer, primary_key=True)
+    starship_id = Column(Integer, primary_key=True)
+    planet_id = Column(Integer, primary_key=True)
+
 class User(Base):
     __tablename__ = 'user'
-     # Here we define columns for the table User
+    # Here we define columns for the table User
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    userId = Column(Integer, primary_key=True)
     first_name = Column(String(250), nullable=False)
     last_name = Column(String(250), nullable=False)
     email = Column(String(200))
     password = Column(String, primary_key=True)
     register_date = Column(String)
-    available_favs = Column(String)
-    character_id = Column(String(150))
-    starship_id = Column(String(150))
-    planet_id = Column(String(150))
-
 
     def to_dict(self):
         return {}
